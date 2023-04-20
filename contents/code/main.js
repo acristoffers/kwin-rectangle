7ea2a04149fd0fb032351da43d179a3791ac4842
Álan Crístoffer <acristoffers@startmail.com>
2023-04-20 18:58:45 +0200

    Version 1.2.5
    Changes:
    - The name of almost all shortcuts for better sorting. Fixes #11.
    
    Fixes:
    - Window maximized status persisting after resizing. Fixes #12.
    - Window position not correctly saved (caused wrong move behaviour). Fixes #12.
    
    Implements:
    - Stretch commands to change window span. Fixes #13.
    - Stretch commands to make window border touch screen border. Fixes #13.

--- contents/code/main.js
+++ contents/code/main.js
@@ -64,6 +64,8 @@ function manage(index, rs, cs, r, c) {
    * -3: Maximize window's width
    * -4: Maximize window's height
    * -5: Move window in rs,cs direction
+   * -6: Stretch (resizes to make window touch screen side)
+   * -7: Increases window span (rs and cs)
    */
   if (!workspace.activeClient.normalWindow && !workspace.activeClient.utility) {
     return
@@ -83,8 +85,12 @@ function manage(index, rs, cs, r, c) {
 
   if (index >= 0) {
     geometry = geometryForGrid(index, rs, cs, r, c)
+  } else if (index == -1) {
+    geometry = center(geometry)
   } else if (index == -2) {
     geometry = geometryForGrid(1, rs, cs, r, c)
+    geometry = center(geometry)
+    workspace.activeClient.rectangleArgs = [0, rs, cs, r, c]
   } else if (index == -3) {
     const full = geometryForGrid(0, 1, 1, 1, 1)
     geometry.x = full.x
@@ -93,28 +99,56 @@ function manage(index, rs, cs, r, c) {
     const full = geometryForGrid(0, 1, 1, 1, 1)
     geometry.y = full.y
     geometry.height = full.height
-  } else if (index == -5) {
-    if (workspace.activeClient.rectangleArgs != null) {
-      ;[index, prs, pcs, pr, pc] = workspace.activeClient.rectangleArgs
-      let j = index % pc
-      let i = (index - j) / pc
-      j = Math.min(pc - pcs, Math.max(0, j + rs))
-      i = Math.min(pr - prs, Math.max(0, i + cs))
-      index = i * pc + j
-      geometry = geometryForGrid(index, prs, pcs, pr, pc)
-      workspace.activeClient.rectangleArgs = [index, prs, pcs, pr, pc]
-      index = -5
+  } else if (index == -5 && workspace.activeClient.rectangleArgs != null) {
+    ;[idx, prs, pcs, pr, pc] = workspace.activeClient.rectangleArgs
+    let j = idx % pc
+    let i = (idx - j) / pc
+    j = Math.min(pc - pcs, Math.max(0, j + rs))
+    i = Math.min(pr - prs, Math.max(0, i + cs))
+    idx = i * pc + j
+    geometry = geometryForGrid(idx, prs, pcs, pr, pc)
+    workspace.activeClient.rectangleArgs = [idx, prs, pcs, pr, pc]
+  } else if (index == -6) {
+    const screen = screenSize()
+    const pad = paddings()
+    switch (rs) {
+      case 0:
+        geometry.height += geometry.y - pad.outer
+        geometry.y = pad.outer
+        break
+      case 1:
+        geometry.height = screen.height - pad.outer - geometry.y
+        break
+      case 2:
+        geometry.width += geometry.x - pad.outer
+        geometry.x = pad.outer
+        break
+      case 3:
+        geometry.width = screen.width - pad.outer - geometry.x
+        break
     }
+  } else if (index == -7 && workspace.activeClient.rectangleArgs != null) {
+    ;[idx, prs, pcs, pr, pc] = workspace.activeClient.rectangleArgs
+    let j = idx % pc
+    let i = (idx - j) / pc
+    let newj = Math.min(pc - pcs, Math.max(0, j + rs))
+    let newi = Math.min(pr - prs, Math.max(0, i + cs))
+    // Adds colum/row if necessary
+    pcs += j != newj ? 1 : 0
+    prs += i != newi ? 1 : 0
+    // Moves only if newi/newj is smaller (only towards top-left)
+    newj = j < newj ? j : newj
+    newi = i < newi ? i : newi
+    idx = newi * pc + newj
+    geometry = geometryForGrid(idx, prs, pcs, pr, pc)
+    workspace.activeClient.rectangleArgs = [idx, prs, pcs, pr, pc]
   }
 
-  if (index == -1 || index == -2) {
-    geometry = center(geometry)
-  }
-
-  if (index != -5) {
+  if (index >= 0) {
     workspace.activeClient.rectangleArgs = [index, rs, cs, r, c]
   }
 
+  workspace.activeClient.setMaximize(false, false)
   workspace.activeClient.geometry = geometry
 }
 
@@ -147,61 +181,73 @@ function shortcut(text, shortcut, i, rs, cs, r, c) {
   registerShortcut(text, text, shortcut, () => manage(i, rs, cs, r, c))
 }
 
-shortcut('Quarter: Top Left', 'U', 0, 1, 1, 2, 2)
-shortcut('Quarter: Top Right', 'I', 1, 1, 1, 2, 2)
-shortcut('Quarter: Bottom Left', 'J', 2, 1, 1, 2, 2)
-shortcut('Quarter: Bottom Right', 'K', 3, 1, 1, 2, 2)
+shortcut('Quarter: (1) Top Left', 'U', 0, 1, 1, 2, 2)
+shortcut('Quarter: (2) Top Right', 'I', 1, 1, 1, 2, 2)
+shortcut('Quarter: (3) Bottom Left', 'J', 2, 1, 1, 2, 2)
+shortcut('Quarter: (4) Bottom Right', 'K', 3, 1, 1, 2, 2)
+shortcut('Quarter: (5) Centered', 'Alt+C', -2, 1, 1, 2, 2)
 
-shortcut('Fourth: First', 'V', 0, 1, 1, 1, 4)
-shortcut('Fourth: Second', 'B', 1, 1, 1, 1, 4)
-shortcut('Fourth: Third', 'N', 2, 1, 1, 1, 4)
-shortcut('Fourth: Fourth', 'M', 3, 1, 1, 1, 4)
+shortcut('Fourth: (1) First', 'V', 0, 1, 1, 1, 4)
+shortcut('Fourth: (2) Second', 'B', 1, 1, 1, 1, 4)
+shortcut('Fourth: (3) Third', 'N', 2, 1, 1, 1, 4)
+shortcut('Fourth: (4) Fourth', 'M', 3, 1, 1, 1, 4)
 
-shortcut('Thirds: First', 'D', 0, 1, 1, 1, 3)
-shortcut('Thirds: Second', 'F', 1, 1, 1, 1, 3)
-shortcut('Thirds: Third', 'G', 2, 1, 1, 1, 3)
+shortcut('Third: (1) First', 'D', 0, 1, 1, 1, 3)
+shortcut('Third: (2) Second', 'F', 1, 1, 1, 1, 3)
+shortcut('Third: (3) Third', 'G', 2, 1, 1, 1, 3)
 
-shortcut('Sixth: Top Left', 'Shift+U', 0, 1, 1, 2, 3)
-shortcut('Sixth: Top Center', 'Shift+I', 1, 1, 1, 2, 3)
-shortcut('Sixth: Top Right', 'Shift+O', 2, 1, 1, 2, 3)
-shortcut('Sixth: Bottom Left', 'Shift+J', 3, 1, 1, 2, 3)
-shortcut('Sixth: Bottom Center', 'Shift+K', 4, 1, 1, 2, 3)
-shortcut('Sixth: Bottom Right', 'Shift+L', 5, 1, 1, 2, 3)
+shortcut('Sixth: (1) Top Left', 'Shift+U', 0, 1, 1, 2, 3)
+shortcut('Sixth: (2) Top Center', 'Shift+I', 1, 1, 1, 2, 3)
+shortcut('Sixth: (3) Top Right', 'Shift+O', 2, 1, 1, 2, 3)
+shortcut('Sixth: (4) Bottom Left', 'Shift+J', 3, 1, 1, 2, 3)
+shortcut('Sixth: (5) Bottom Center', 'Shift+K', 4, 1, 1, 2, 3)
+shortcut('Sixth: (6) Bottom Right', 'Shift+L', 5, 1, 1, 2, 3)
 
-shortcut('Ninth: Top Left', 'Alt+U', 0, 1, 1, 3, 3)
-shortcut('Ninth: Top Center', 'Alt+I', 1, 1, 1, 3, 3)
-shortcut('Ninth: Top Right', 'Alt+O', 2, 1, 1, 3, 3)
-shortcut('Ninth: Middle Left', 'Alt+J', 3, 1, 1, 3, 3)
-shortcut('Ninth: Middle Center', 'Alt+K', 4, 1, 1, 3, 3)
-shortcut('Ninth: Middle Right', 'Alt+L', 5, 1, 1, 3, 3)
-shortcut('Ninth: Bottom Left', 'Alt+N', 6, 1, 1, 3, 3)
-shortcut('Ninth: Bottom Center', 'Alt+M', 7, 1, 1, 3, 3)
-shortcut('Ninth: Bottom Right', 'Alt+,', 8, 1, 1, 3, 3)
+shortcut('Ninth: (1) Top Left', 'Alt+U', 0, 1, 1, 3, 3)
+shortcut('Ninth: (2) Top Center', 'Alt+I', 1, 1, 1, 3, 3)
+shortcut('Ninth: (3) Top Right', 'Alt+O', 2, 1, 1, 3, 3)
+shortcut('Ninth: (4) Middle Left', 'Alt+J', 3, 1, 1, 3, 3)
+shortcut('Ninth: (5) Middle Center', 'Alt+K', 4, 1, 1, 3, 3)
+shortcut('Ninth: (6) Middle Right', 'Alt+L', 5, 1, 1, 3, 3)
+shortcut('Ninth: (7) Bottom Left', 'Alt+N', 6, 1, 1, 3, 3)
+shortcut('Ninth: (8) Bottom Center', 'Alt+M', 7, 1, 1, 3, 3)
+shortcut('Ninth: (9) Bottom Right', 'Alt+,', 8, 1, 1, 3, 3)
 
-shortcut('Halves: Center (Vertical)', 'Shift+C', -2, 1, 1, 1, 2)
-shortcut('Halves: Center (Horizontal)', 'Shift+V', -2, 1, 1, 2, 1)
-shortcut('Halves: Left', 'Left', 0, 1, 1, 1, 2)
-shortcut('Halves: Right', 'Right', 1, 1, 1, 1, 2)
-shortcut('Halves: Top', 'Up', 0, 1, 1, 2, 1)
-shortcut('Halves: Bottom', 'Down', 1, 1, 1, 2, 1)
+shortcut('Half: (1) Center (Vertical)', 'Shift+C', -2, 1, 1, 1, 2)
+shortcut('Half: (1) Center (Horizontal)', 'Shift+V', -2, 1, 1, 2, 1)
+shortcut('Half: (2) Left', 'Left', 0, 1, 1, 1, 2)
+shortcut('Half: (2) Right', 'Right', 1, 1, 1, 1, 2)
+shortcut('Half: (3) Top', 'Up', 0, 1, 1, 2, 1)
+shortcut('Half: (3) Bottom', 'Down', 1, 1, 1, 2, 1)
 
-shortcut('Two Thirds: First', 'E', 0, 1, 2, 1, 3)
-shortcut('Two Thirds: Second', 'T', 1, 1, 2, 1, 3)
-shortcut('Two Thirds: Center', 'R', -2, 1, 2, 1, 3)
+shortcut('Two Thirds: (1) Left', 'E', 0, 1, 2, 1, 3)
+shortcut('Two Thirds: (2) Center', 'R', -2, 1, 2, 1, 3)
+shortcut('Two Thirds: (3) Right', 'T', 1, 1, 2, 1, 3)
 
 shortcut('Center', 'C', -1, 1, 1, 1, 1)
-shortcut('Maximized', 'Return', 0, 1, 1, 1, 1)
-shortcut('Centered Quarter', 'Alt+C', -2, 1, 1, 2, 2)
-shortcut('Almost Maximized', 'Shift+Return', 33, 30, 30, 32, 32)
+shortcut('Maximize', 'Return', 0, 1, 1, 1, 1)
+shortcut('Maximize: Almost', 'Shift+Return', 33, 30, 30, 32, 32)
+shortcut('Maximize: Height', 'Shift+Alt+Up', -4, 0, 0, 0, 0)
+shortcut('Maximize: Width', 'Shift+Alt+Right', -3, 0, 0, 0, 0)
 
-shortcut('Maximize Height', 'Shift+Alt+Up', -4, 0, 0, 0, 0)
-shortcut('Maximize Width', 'Shift+Alt+Right', -3, 0, 0, 0, 0)
+shortcut('Stretch: (1) Top', 'Alt+Up', -6, 0, 0, 0, 0)
+shortcut('Stretch: (1) Bottom', 'Alt+Down', -6, 1, 0, 0, 0)
+shortcut('Stretch: (2) Left', 'Alt+Left', -6, 2, 0, 0, 0)
+shortcut('Stretch: (2) Right', 'Alt+Right', -6, 3, 0, 0, 0)
+shortcut('Stretch: Step: (1) Bottom Left', '1', -7, -1, 1, 1, 1)
+shortcut('Stretch: Step: (2) Bottom', '2', -7, 0, 1, 1, 1)
+shortcut('Stretch: Step: (3) Bottom Right', '3', -7, 1, 1, 1, 1)
+shortcut('Stretch: Step: (4) Left', '4', -7, -1, 0, 1, 1)
+shortcut('Stretch: Step: (5) Right', '6', -7, 1, 0, 1, 1)
+shortcut('Stretch: Step: (6) Top Left', '7', -7, -1, -1, 1, 1)
+shortcut('Stretch: Step: (7) Top', '8', -7, 0, -1, 1, 1)
+shortcut('Stretch: Step: (8) Top Right', '9', -7, 1, -1, 1, 1)
 
-shortcut('Move: Left', 'Alt+4', -5, -1, 0, 1, 1)
-shortcut('Move: Right', 'Alt+6', -5, 1, 0, 1, 1)
-shortcut('Move: Top', 'Alt+8', -5, 0, -1, 1, 1)
-shortcut('Move: Bottom', 'Alt+2', -5, 0, 1, 1, 1)
-shortcut('Move: Top Left', 'Alt+7', -5, -1, -1, 1, 1)
-shortcut('Move: Top Right', 'Alt+9', -5, 1, -1, 1, 1)
-shortcut('Move: Bottom Left', 'Alt+1', -5, -1, 1, 1, 1)
-shortcut('Move: Bottom Right', 'Alt+3', -5, 1, 1, 1, 1)
+shortcut('Move: (1) Bottom Left', 'Alt+1', -5, -1, 1, 1, 1)
+shortcut('Move: (2) Bottom', 'Alt+2', -5, 0, 1, 1, 1)
+shortcut('Move: (3) Bottom Right', 'Alt+3', -5, 1, 1, 1, 1)
+shortcut('Move: (4) Left', 'Alt+4', -5, -1, 0, 1, 1)
+shortcut('Move: (5) Right', 'Alt+6', -5, 1, 0, 1, 1)
+shortcut('Move: (6) Top Left', 'Alt+7', -5, -1, -1, 1, 1)
+shortcut('Move: (7) Top', 'Alt+8', -5, 0, -1, 1, 1)
+shortcut('Move: (8) Top Right', 'Alt+9', -5, 1, -1, 1, 1)
